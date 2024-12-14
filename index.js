500fca3700be00616727cb32354ae454bb9fc549
lukejbassett <bassettlukej@gmail.com>
2024-12-14 02:36:47 +0000

    first commit for learning js through a slot machine

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
-const testing = true;
+// js learning through slot machine
 
-const newBranchTest = true;
+// tasks to do vvv
+// deposit money DONE
+// determine lines on bet DONE
+// collect bet amount
+// spin slot machine
+// check user winnings
+// give winnings / take bet
+// leave or play again (force quit with no money)
+
+// prompt package
+const prompt = require("prompt-sync")();
+
+// es6 function
+const depositAmount = () => {
+  while (true) {
+    const depositAmount = prompt("Enter your deposit amount: ");
+    const depositAmountNumber = parseFloat(depositAmount);
+
+    if (isNaN(depositAmountNumber) || depositAmountNumber <= 0) {
+      console.log("Invalid amount, try again.");
+    } else {
+      return depositAmountNumber;
+    }
+  }
+};
+
+const getNumberOfLines = () => {
+  while (true) {
+    const lines = prompt("How many lines do you want to bet: ");
+    const numberOfLines = parseFloat(lines);
+    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+      console.log("Invalid amount, try again.");
+    } else {
+      return numberOfLines;
+    }
+  }
+};
+
+depositAmount();
+getNumberOfLines();
